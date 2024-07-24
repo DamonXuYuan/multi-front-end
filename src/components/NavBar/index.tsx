daf0e8f5114b24341fffce11356e70225e7bee11
unknown <geeksams@gmail.com>
2024-07-24 10:28:57 +0800

    feat: navbar,tabbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import { Box, Flex, Heading, BoxProps } from '@chakra-ui/react';
+import React from 'react'
+import { Box, Flex, Heading, BoxProps } from '@chakra-ui/react'
 
 interface TopNavbarProps extends BoxProps {
-  leftContent?: React.ReactNode;
-  rightContent?: React.ReactNode;
-  title: string;
-  isFixed?: boolean;
+  leftContent?: React.ReactNode
+  rightContent?: React.ReactNode
+  title: string
+  isFixed?: boolean
 }
 
-const Navbar = ({ 
-  leftContent, 
-  rightContent, 
-  title, 
-  isFixed = false, 
-  ...rest 
-}: TopNavbarProps) => {
+const Navbar = ({ leftContent, rightContent, title, isFixed = false, ...rest }: TopNavbarProps) => {
   return (
     <Box
       as="nav"
@@ -22,24 +16,24 @@ const Navbar = ({
       boxShadow="md"
       width="100%"
       zIndex={1000}
-      {...(isFixed ? {
-        position: "fixed",
-        top: 0,
-        left: 0,
-      } : {})}
+      {...(isFixed
+        ? {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+          }
+        : {})}
       {...rest}
     >
-      <Flex 
-        justify="space-between" 
-        align="center" 
-        maxWidth="container.xl" 
+      <Flex
+        justify="space-between"
+        align="center"
+        maxWidth="container.xl"
         margin="0 auto"
         height="44px"
         px={4}
       >
-        <Box width="80px">
-          {leftContent}
-        </Box>
+        <Box width="80px">{leftContent}</Box>
         <Heading size="md" textAlign="center" flex={1} color="black">
           {title}
         </Heading>
@@ -48,7 +42,7 @@ const Navbar = ({
         </Box>
       </Flex>
     </Box>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,36 @@
-import React from 'react';
-import { Box, VStack, Text, Image, Flex, Icon } from '@chakra-ui/react';
-import { Bell } from 'lucide-react';
-import Navbar from '@/components/NavBar';
-import TabBar from '@/components/TabBar';
-import MhBanner from '@/assets/imgs/mh_banner.png';
+import React from 'react'
+import { Box, VStack, Text, Image, Flex, Icon } from '@chakra-ui/react'
+import { Bell } from 'lucide-react'
+import Navbar from '@/components/NavBar'
+import TabBar from '@/components/TabBar'
+import MhBanner from '@/assets/imgs/mh_banner.png'
 const HomePage = () => {
   return (
     <Box margin="auto" pt="44px" pb="48px" bg="gray.50" minHeight="100vh">
-      <Navbar title="首页" isFixed={true}/>
+      <Navbar title="首页" isFixed={true} />
       <VStack align="stretch" px={4} mt={4}>
         <Box bg="gray.800" borderRadius="lg" color="white" position="relative" overflow="hidden">
-          <Image 
-            src={MhBanner}
-            alt="Blind Box"
-            objectFit="contain"
-            width="100%"
-            height="150px"
-          />
+          <Image src={MhBanner} alt="Blind Box" objectFit="contain" width="100%" height="150px" />
         </Box>
         <Flex align="center" bg="white" p={3} borderRadius="md" boxShadow="sm" mt={5} mb={8}>
           <Icon as={Bell} mr={2} color="blue.500" />
           <Text color="gray.500">平台更新通知</Text>
         </Flex>
         <Box>
-          <Text color="black" fontSize="lg" fontWeight="bold" mb={2}>盲盒NFT</Text>
+          <Text color="black" fontSize="lg" fontWeight="bold" mb={2}>
+            盲盒NFT
+          </Text>
           <Box width="100%" bg="white" borderRadius="xl" overflow="hidden">
-            <Image
-              src={MhBanner}
-              objectFit="cover"
-              width="100%"
-              height="241px"
-            />
-            <Text color="black" fontSize="lg" fontWeight="bold" py={5} px={3}>多维宇宙</Text>
+            <Image src={MhBanner} objectFit="cover" width="100%" height="241px" />
+            <Text color="black" fontSize="lg" fontWeight="bold" py={5} px={3}>
+              多维宇宙
+            </Text>
           </Box>
         </Box>
       </VStack>
-      <TabBar/>
+      <TabBar />
     </Box>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
