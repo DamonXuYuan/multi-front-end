3de7a1cab8034aae08211acb4d03458469d61c01
Damon <330718757@qq.com>
2024-08-02 04:35:16 +0800

    todo: raffle

--- src/pages/invite.tsx
+++ src/pages/invite.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Box, VStack, Text, Image, HStack, useToast } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import Navbar from '@/components/NavBar'
