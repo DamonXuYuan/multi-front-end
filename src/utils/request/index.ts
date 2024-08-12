d546099375ad1ccf193f0340e3e45ff584153405
unknown <geeksams@gmail.com>
2024-08-12 22:16:36 +0800

    chore: 生成域名

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -92,7 +92,7 @@ let baseUrl
 if (process.env.NODE_ENV === 'development') {
   baseUrl = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  baseUrl = '/'
+  baseUrl = 'http://api.gxsccw.com/'
 }
 if (typeof window !== 'undefined') {
   console.log('window', window)
